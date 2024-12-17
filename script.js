6401fc78d3d4a9e1e0f703bf2a1ff106b9f65e54
Arthur Romanha <126591597+ArthurRomanha@users.noreply.github.com>
2024-12-17 12:35:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-let fundos = [
-    { "ticker": "gare11", cotacao: "", pvp: "", precoJusto: "", valueDividendYeldTwelveMonths: "", lastDividend: "" },
-    { "ticker": "ggrc11", cotacao: "", pvp: "", precoJusto: "", valueDividendYeldTwelveMonths: "", lastDividend: "" },
-    { "ticker": "trxf11", cotacao: "", pvp: "", precoJusto: "", valueDividendYeldTwelveMonths: "", lastDividend: "" },
-    { "ticker": "tepp11", cotacao: "", pvp: "", precoJusto: "", valueDividendYeldTwelveMonths: "", lastDividend: "" }
-];
+const fundosElement = document.getElementById("fundos");
+let fundos = [];
+let table = document.getElementById("data");//select table to show data
+const enviaDados = () => {
+    table.innerHTML = `
+        <tbody>
+            <tr>
+                <th>Ticker</th>
+                <th>Cotação</th>
+                <th>P/VP</th>
+                <th>Preço Justo</th>
+                <th>DY 12M</th>
+                <th>Último Dividendo</th>
+            </tr>
+        </tbody>`;
+    fundosTicker = document.querySelectorAll(".fundo");
+    fundos = [];
+    for (let fundo of fundosTicker) {
+        if ((fundo.textContent.length == 6) && (fundo.textContent.endsWith("11"))) {
+            fundos.push({ "ticker": `${fundo.textContent.toUpperCase()}`, cotacao: "", pvp: "", precoJusto: "", valueDividendYeldTwelveMonths: "", lastDividend: "" });
+        }
+    }
+    fetchCotacao();
+}
+
 async function fetchCotacao() {
     try {
         const response = await fetch('https://api-invest-pi.vercel.app/', {//fetch of my own api
@@ -16,18 +35,16 @@ async function fetchCotacao() {
 
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
-        } 
-        
+        }
+
         const data = await response.json();
         console.log(data);
         let fundosAtualizados = data.fundosAtualizados;
-
-        let table = document.getElementById("data");//select table to show data
-        for (let i = 0;i<fundosAtualizados.length;i++){
+        for (let i = 0; i < fundosAtualizados.length; i++) {
             //show data on the table
             table.innerHTML += `
             <tr>
-                <td>${fundosAtualizados[i].ticker.toUpperCase()}</td>
+                <td>${fundosAtualizados[i].ticker}</td>
                 <td>${fundosAtualizados[i].cotacao}</td>
                 <td>${fundosAtualizados[i].pvp}</td>
                 <td>${fundosAtualizados[i].precoJusto}</td>
@@ -36,9 +53,27 @@ async function fetchCotacao() {
             </tr>
             `
         }
-        
+
     } catch (error) {
         console.error('Error colecting data', error);
     }
 }
-fetchCotacao();
+const removeItem = (evento) => {
+
+    const elemento = evento.target;
+
+    if (elemento.name == "remove") {
+        console.log(elemento);
+
+        elemento.parentElement.remove();
+    }
+}
+const addItem = (evento) => {
+
+    const elemento = evento.target;
+    if (elemento.name == "newFundo") {
+        fundosElement.innerHTML += `<div class="container-fundo"><button name="remove">&#10060;</button><div contenteditable="true" class="fundo"></div></div>`;
+    }
+}
+document.addEventListener("click", removeItem);
+document.addEventListener("click", addItem);

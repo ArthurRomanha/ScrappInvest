5b1fe89ebee20e1e6c27f7a6e08c9eeaf1d002e0
Arthur Romanha <126591597+ArthurRomanha@users.noreply.github.com>
2025-01-27 16:35:42 -0300

    Release 5.0.0

--- script.js
+++ script.js
@@ -129,8 +129,8 @@ function sortTable(columnIndex) {
 
         if (isNumericColumn) {
             // Remove cifrões e converte para números
-            const numA = parseFloat(cellA.replace(/[$]/g, '').replace(/,/g, ''));
-            const numB = parseFloat(cellB.replace(/[$]/g, '').replace(/,/g, ''));
+            const numA = parseFloat(cellA.replace(/[R$]/g, '').replace(/,/g, ''));
+            const numB = parseFloat(cellB.replace(/[R$]/g, '').replace(/,/g, ''));
 
             if (sortDirection) {
                 return numA - numB; // Ordena numericamente em ordem crescente
